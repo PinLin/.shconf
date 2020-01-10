0b77d1f98723871065cfec0e1b513879a5640aa7
Pin Lin <moneycat711@gmail.com>
2020-01-10 15:43:15 +0800

    Format install.sh

--- install.sh
+++ install.sh
@@ -7,17 +7,14 @@ REPO_VERSION="v3.1"
 # Install application
 makeInstall() {
     # Check counts of arguments
-    if [ $# -lt 1 ]
-    then
+    if [ $# -lt 1 ]; then
         return -1
     fi
 
     # Judge which is the package manager we used
     kernel=$(uname -s)
-    if [ "$kernel" = "Darwin" ]
-    then
-        if command -v brew > /dev/null 2>&1
-        then
+    if [ "$kernel" = "Darwin" ]; then
+        if command -v brew >/dev/null 2>&1; then
             # macOS with brew
             brew install $1
             return $?
@@ -27,10 +24,8 @@ makeInstall() {
             return 87
         fi
 
-    elif [ "$kernel" = "FreeBSD" ]
-    then
-        if command -v pkg > /dev/null 2>&1
-        then
+    elif [ "$kernel" = "FreeBSD" ]; then
+        if command -v pkg >/dev/null 2>&1; then
             # FreeBSD with pkg
             sudo pkg install -y $1
             return $?
@@ -40,30 +35,24 @@ makeInstall() {
             return 87
         fi
 
-    elif [ "$kernel" = "Linux" ]
-    then
-        if command -v apt-get > /dev/null 2>&1
-        then
+    elif [ "$kernel" = "Linux" ]; then
+        if command -v apt-get >/dev/null 2>&1; then
             # Debian/Ubuntu with apt-get
             sudo apt-get install -y $1
             return $?
-        elif command -v dnf > /dev/null 2>&1
-        then
+        elif command -v dnf >/dev/null 2>&1; then
             # Fedora/CentOS with dnf
             sudo dnf install -y $1
             return $?
-        elif command -v yum > /dev/null 2>&1
-        then
+        elif command -v yum >/dev/null 2>&1; then
             # Fedora/CentOS with yum
             sudo yum install -y $1
             return $?
-        elif command -v ipkg > /dev/null 2>&1
-        then
+        elif command -v ipkg >/dev/null 2>&1; then
             # Embedded Device with ipkg
             sudo ipkg install $1
             return $?
-        elif command -v opkg > /dev/null 2>&1
-        then
+        elif command -v opkg >/dev/null 2>&1; then
             # Embedded Device with opkg
             sudo opkg install $1
             return $?
@@ -71,64 +60,58 @@ makeInstall() {
     fi
 }
 
-
 # Ask for question
 askQuestion() {
     # Check counts of arguments
-    if [ $# -lt 2 ]
-    then
+    if [ $# -lt 2 ]; then
         return -1
     fi
 
     # Ask
-    if [ "$2" = "Yn" ]
-    then
+    if [ "$2" = "Yn" ]; then
         # Display question and default yes
-        printf "$1 [Y/n] "; read ans
+        printf "$1 [Y/n] "
+        read ans
         case $ans in
-            [Nn*])
-                return 1
-                ;;
-            *)
-                return 0
-                ;;
+        [Nn*])
+            return 1
+            ;;
+        *)
+            return 0
+            ;;
         esac
     else
         # Display question and default no
-        printf "$1 [y/N] "; read ans
+        printf "$1 [y/N] "
+        read ans
         case $ans in
-            [Yy*])
-                return 0
-                ;;
-            *)
-                return 1
-                ;;
+        [Yy*])
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
         esac
     fi
 }
 
-
 main() {
     # Check git
-    if ! command -v git > /dev/null 2>&1
-    then
+    if ! command -v git >/dev/null 2>&1; then
         echo "This installer uses git to clone the configs to localhost."
-        if askQuestion "Do you want to install git?" "Yn"
-        then
+        if askQuestion "Do you want to install git?" "Yn"; then
             makeInstall git
         fi
     fi
 
     # Remove old one
-    if [ -d ~/$REPO_NAME ]
-    then
+    if [ -d ~/$REPO_NAME ]; then
         rm -rf ~/$REPO_NAME
     fi
 
     # Clone repo to local
     git clone $REPO_URL ~/$REPO_NAME
-    if [ $? != 0 ]
-    then
+    if [ $? != 0 ]; then
         echo "Failed to clone $REPO_NAME."
         return 1
     fi
@@ -138,79 +121,64 @@ main() {
     # Ask for installing
     todo=''
     apps='zsh vim tmux'
-    for app in $apps
-    do
-        if ! command -v $app > /dev/null 2>&1
-        then
+    for app in $apps; do
+        if ! command -v $app >/dev/null 2>&1; then
             msg="Do you want to install and apply configs about $app?"
         else
             msg="Do you want to apply configs about $app?"
         fi
 
-        if askQuestion "$msg" "Yn"
-        then
+        if askQuestion "$msg" "Yn"; then
             todo="$todo $app"
         fi
     done
-    if [ "$todo" != "" ]
-    then
+    if [ "$todo" != "" ]; then
         makeInstall $todo
     fi
 
     # Apply configs about zsh
-    if echo $todo | grep zsh > /dev/null
-    then
+    if echo $todo | grep zsh >/dev/null; then
         # Require oh-my-zsh
-        if ! [ -d ~/.oh-my-zsh ]
-        then
-            if command -v curl > /dev/null 2>&1
-            then
+        if ! [ -d ~/.oh-my-zsh ]; then
+            if command -v curl >/dev/null 2>&1; then
                 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's/env zsh -l//g')"
             else
                 sh -c "$(wget -qO- https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's/env zsh -l//g')"
             fi
         fi
         # Install powerlevel9k
-        if ! [ -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]
-        then
+        if ! [ -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]; then
             git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
         fi
         # Install zsh-autosuggestions
-        if ! [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]
-        then
+        if ! [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
             git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
         fi
         # Install zsh-syntax-highlighting
-        if ! [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]
-        then
+        if ! [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
             git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
         fi
-        if [ -f ~/.zshrc ]
-        then
+        if [ -f ~/.zshrc ]; then
             mv ~/.zshrc ~/.zshrc.bak
         fi
-        echo "source ~/$REPO_NAME/config/zsh/sample.zshrc" >> ~/.zshrc
-        echo "DEFAULT_USER=$USER" >> ~/.zshrc
+        echo "source ~/$REPO_NAME/config/zsh/sample.zshrc" >>~/.zshrc
+        echo "DEFAULT_USER=$USER" >>~/.zshrc
     fi
 
     # Apply configs about vim
-    if echo $todo | grep vim > /dev/null
-    then
-        if [ -f ~/.vimrc ]
-        then
+    if echo $todo | grep vim >/dev/null; then
+        if [ -f ~/.vimrc ]; then
             mv ~/.vimrc ~/.vimrc.bak
         fi
-        echo "source ~/$REPO_NAME/config/vim/sample.vimrc" >> ~/.vimrc
+        echo "source ~/$REPO_NAME/config/vim/sample.vimrc" >>~/.vimrc
     fi
 
     # Apply configs about tmux
-    if echo $todo | grep tmux > /dev/null
-    then
-        if [ -f ~/.tmux.conf ]
-        then
+    if echo $todo | grep tmux >/dev/null; then
+        if [ -f ~/.tmux.conf ]; then
             mv ~/.tmux.conf ~/.tmux.conf.bak
         fi
-        echo "source ~/$REPO_NAME/config/tmux/sample.tmux.conf" >> ~/.tmux.conf
+        echo "source ~/$REPO_NAME/config/tmux/sample.tmux.conf" >>~/.tmux.conf
     fi
 
     # Finished
